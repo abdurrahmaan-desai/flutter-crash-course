044b5ed49805a6c961b32f52d08aa22b611c7dfd
Abdurrahmaan <amsdprivate@gmail.com>
2023-01-26 12:58:56 +0000

    Lesson 7 changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'location_detail.dart';
 import 'mocks/mock_location.dart';
 import 'models/location.dart';
+import 'location_list.dart';
 
 void main() {
   // runApp(const MyApp());
 
-  final Location mockLocation = MockLocation.FetchAny();
+  final mockLocations = MockLocation.fetchAll();
 
   return runApp(MaterialApp(
-    home: LocationDetail(mockLocation)));
+    home: LocationList(mockLocations)));
 
 }
